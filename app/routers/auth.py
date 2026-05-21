82ac6e89484d7a8d3f85d0db022f27e8e0caa305
dragondangun <dan.sura@mail.ru>
2026-05-21 13:46:18 +0300

    Fix 404 -> 403

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -15,11 +15,11 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
         .filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Invalid Credentials")
 
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Invalid Credentials")
 
     access_token = oauth2.create_access_token(data={"user_id": user.id})
