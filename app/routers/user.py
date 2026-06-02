8e63cba890a9d10645ddbed86dd2d60081818f4e
dragondangun <dan.sura@mail.ru>
2026-06-02 16:12:02 +0300

    email unique check [skip ci]

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,5 @@
 # from http.client import TOO_EARLY
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from fastapi import Depends, HTTPException, status, APIRouter
@@ -14,12 +15,32 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/", status_code=status.HTTP_201_CREATED,
              response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    existing_user = (
+        db.query(models.User)
+        .filter(models.User.email == user.email)
+        .first()
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this email already exists",
+        )
+
     user_data = user.model_dump()
     user_data["password"] = get_password_hash(user.password)
 
     new_user = models.User(**user_data)
-    db.add(new_user)
-    db.commit()
+    try:
+        db.add(new_user)
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this email already exists",
+        )
+
     db.refresh(new_user)
 
     return new_user
