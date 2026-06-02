53e72ed8c69a979ae8d9cb281a81a3efc98c005e
dragondangun <dan.sura@mail.ru>
2026-06-02 15:41:11 +0300

    do not mutate data [skip ci]

--- app/routers/user.py
+++ app/routers/user.py
@@ -14,10 +14,10 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/", status_code=status.HTTP_201_CREATED,
              response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    hashed_password = get_password_hash(user.password)
-    user.password = hashed_password
+    user_data = user.model_dump()
+    user_data["password"] = get_password_hash(user.password)
 
-    new_user = models.User(**user.model_dump())
+    new_user = models.User(**user_data)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
